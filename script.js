459f9724718e043adb924687a0ed07dc6ab59e6c
Uday-cpu16 <udaysn1601@gmail.com>
2026-08-12 00:09:33 +0530

    Changes made to html css and javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("text-box");
+const list = document.getElementById("list-container");
+
+function addGoal(){
+    if (inputBox.value === ''){
+        alert("Wrong!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.append(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.append(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+list.addEventListener("click", (e) =>{
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData() {
+    localStorage.setItem("data",list.innerHTML);
+}
+
+function showTasks() {
+    list.innerHTML = localStorage.getItem("data");
+}
+
+showTasks();
