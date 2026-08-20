aab5a92dca74e4f087b3bde5498a0aa496eb9d61
Harshs2001 <udaysn1601@gmail.com>
2026-08-21 00:43:48 +0530

    Added Dayzone buttons and made minor inmprovements

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ function addGoal(){
         alert("Wrong!");
     }
     else{
-        let li = document.createElement("li");
+        let li = document.createElement("li");      
         li.innerHTML = inputBox.value;
         list.append(li);
+
+        let dayBtn = document.createElement("button");
+        // dayBtn.textContent = "Select Time";
+        dayBtn.className = "dayBtn";
+        li.append(dayBtn);
+
+        // let dayImg = document.createElement("div");
+
+        let dayImg = document.createElement("img");
+        dayImg.className = "dayImg";
+        li.append(dayImg);
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.append(span);
@@ -17,7 +29,12 @@ function addGoal(){
     saveData();
 }
 
+
+let index = 0;
+
 list.addEventListener("click", (e) =>{
+    let dayZones = ["early_morning.png","morning.png","afternoon.png","evening.png","night.png"];
+    let dayTitles = ["early_morning_title.png","morning_title.png","afternoon_title.png","evening_title.png","night_title.png"];
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
@@ -25,6 +42,19 @@ list.addEventListener("click", (e) =>{
     else if(e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
+    }
+    else if(e.target.tagName === "BUTTON") {
+
+        let currentIndex = parseInt(e.target.dataset.index || "0");
+        currentIndex = (currentIndex + 1) % (dayZones.length);
+        e.target.dataset.index = currentIndex;
+        e.target.style.backgroundImage = `url(./icons/${dayZones[currentIndex]})`;
+
+        let imgDay = e.target.closest("li").querySelector(".dayImg");
+        if (imgDay) {
+           imgDay.style.backgroundImage = `url(./icons/${dayTitles[currentIndex]})`;
+        }
+        
     }
 },false);
 
